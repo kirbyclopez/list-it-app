ae9a81ff44f8c8b347f23d20ef9de06f81e814df
kirbyclopez <kirbyclopez@gmail.com>
2023-01-21 11:35:05 +0800

    feat: updated Footer component

--- components/layouts/footer/Footer.tsx
+++ components/layouts/footer/Footer.tsx
@@ -1,17 +1,27 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import klcodesLogo from '../../../public/images/klcodes-logo-primary-text.png';
+
 export interface IFooter extends React.ComponentPropsWithoutRef<'footer'> {}
 
 const Footer: React.FC<IFooter> = ({ ...footerProps }) => {
   return (
     <footer
       {...footerProps}
-      className="footer footer-center w-full p-4 bg-gray-300 text-gray-800"
+      className="footer footer-center w-full p-4 bg-gray-100 text-gray-400"
     >
       <div className="text-center">
         <p>
-          Copyright © 2023 - {''}
-          <a className="font-semibold" href="mailto:kirbyclopez@gmail.com">
-            klcodes
-          </a>
+          <Link href="https://www.klcodes.com/">
+            <Image
+              src={klcodesLogo}
+              alt="klcodes Logo"
+              height={25}
+              width={25}
+              className="inline-block mt-[-2px] mr-1"
+            />
+          </Link>
+          © 2023 klcodes
         </p>
       </div>
     </footer>
