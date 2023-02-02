579fbad7758a81366caf28b3fdd6af8a47ad81d8
kirbyclopez <kirbyclopez@gmail.com>
2023-02-02 17:29:40 +0800

    feat: updated AddItemForm.stories

--- components/forms/add-item-form/AddItemForm.mocks.ts
+++ components/forms/add-item-form/AddItemForm.mocks.ts
@@ -1,6 +1,8 @@
 import { IAddItemForm } from './AddItemForm';
 
-const base: IAddItemForm = {};
+const base: IAddItemForm = {
+  listId: '63c7e5ef1a6cdd7f785dbe8d',
+};
 
 export const mockAddItemFormProps = {
   base,
